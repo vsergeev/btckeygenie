8ae46f189523b3e4c5ce909e864364a92a15c067
Vanya Sergeev <vsergeev@gmail.com>
2014-11-24 02:39:25 -0800

    separate Base58 from Base58Check, add decoding and unit tests

--- btckey/btckey.go
+++ btckey/btckey.go
@@ -7,20 +7,18 @@
 package btckey
 
 import (
+	"bytes"
 	"code.google.com/p/go.crypto/ripemd160"
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
 	"math/big"
+	"strings"
 )
 
 /******************************************************************************/
 /* Bitcoin Keypair Generation */
 /******************************************************************************/
-
-/* We make our own Bitcoin Private Key and Public Key structs here so no one
- * tries to use this code with crypto/ecdsa, which doesn't support the
- * secp256k1 elliptic curve that bitcoin uses */
 
 // PublicKey represents a Bitcoin public key.
 type PublicKey struct {
@@ -53,7 +51,7 @@ func GenerateKey() (priv PrivateKey, err error) {
 	/* Random integer uniformly selected from [0, n-1) range */
 	d, err := rand.Int(rand.Reader, new(big.Int).Sub(curve.N, big.NewInt(1)))
 	if err != nil {
-		return priv, fmt.Errorf("Error: generating random private key.")
+		return priv, fmt.Errorf("Generating random private key.")
 	}
 	/* Add one to shift d to [1, n) range */
 	d.Add(d, big.NewInt(1))
@@ -63,7 +61,7 @@ func GenerateKey() (priv PrivateKey, err error) {
 
 	/* Check that Q is on the curve */
 	if !curve.IsOnCurve(Q) {
-		return priv, fmt.Errorf("Error: catastrophic math logic failure.")
+		return priv, fmt.Errorf("Catastrophic math logic failure.")
 	}
 
 	priv.D = d
@@ -77,20 +75,22 @@ func GenerateKey() (priv PrivateKey, err error) {
 /* Bitcoin Public and Private Key Export Mechanics */
 /******************************************************************************/
 
-// Base58 computes the base-58 encoding of a bytes slice b.
-func Base58(b []byte) (s string) {
+// b58encode encodes a bytes slice b into a base-58 encoded string.
+func b58encode(b []byte) (s string) {
 	/* See https://en.bitcoin.it/wiki/Base58Check_encoding */
 
 	const BITCOIN_BASE58_TABLE = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
-	/* Initialize */
+	/* Convert big endian bytes to big int */
 	x := new(big.Int).SetBytes(b)
+
+	/* Initialize */
 	r := new(big.Int)
 	m := big.NewInt(58)
 	zero := big.NewInt(0)
 	s = ""
 
-	/* While x > 0 */
+	/* Convert big int to string */
 	for x.Cmp(zero) > 0 {
 		/* x, r = (x / 58, x % 58) */
 		x.QuoRem(x, m, r)
@@ -98,48 +98,129 @@ func Base58(b []byte) (s string) {
 		s = string(BITCOIN_BASE58_TABLE[r.Int64()]) + s
 	}
 
+	return s
+}
+
+// b58decode decodes a base-58 encoded string into a bytes slice b.
+func b58decode(s string) (b []byte, err error) {
+	/* See https://en.bitcoin.it/wiki/Base58Check_encoding */
+
+	const BITCOIN_BASE58_TABLE = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+	/* Initialize */
+	x := big.NewInt(0)
+	m := big.NewInt(58)
+
+	/* Convert string to big int */
+	for i := 0; i < len(s); i++ {
+		b58index := strings.IndexByte(BITCOIN_BASE58_TABLE, s[i])
+		if b58index == -1 {
+			return nil, fmt.Errorf("Invalid base-58 character encountered: '%c', index %d.", s[i], i)
+		}
+		b58value := big.NewInt(int64(b58index))
+		x.Mul(x, m)
+		x.Add(x, b58value)
+	}
+
+	/* Convert big int to big endian bytes */
+	b = x.Bytes()
+
+	return b, nil
+}
+
+// b58checkencode encodes version ver and bytes slice b into a base-58 check encoded string.
+func b58checkencode(ver uint8, b []byte) (s string) {
+	/* Prepend version */
+	bcpy := append([]byte{ver}, b...)
+
+	/* Create a new SHA256 context */
+	sha256_h := sha256.New()
+
+	/* SHA256 Hash #1 */
+	sha256_h.Reset()
+	sha256_h.Write(bcpy)
+	hash1 := sha256_h.Sum(nil)
+
+	/* SHA256 Hash #2 */
+	sha256_h.Reset()
+	sha256_h.Write(hash1)
+	hash2 := sha256_h.Sum(nil)
+
+	/* Append first four bytes of hash */
+	bcpy = append(bcpy, hash2[0:4]...)
+
+	/* Encode base58 string */
+	s = b58encode(bcpy)
+
 	/* For number of leading 0's in bytes, prepend 1 */
-	for _, v := range b {
+	for _, v := range bcpy {
 		if v != 0 {
 			break
 		}
-		s = string(BITCOIN_BASE58_TABLE[0]) + s
+		s = "1" + s
 	}
 
 	return s
+}
+
+// b58checkdecode decodes base-58 check encoded string s into a version ver and bytes slice b.
+func b58checkdecode(s string) (ver uint8, b []byte, err error) {
+	/* Decode base58 string */
+	b, err = b58decode(s)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	/* Add leading zero bytes */
+	for i := 0; i < len(s); i++ {
+		if s[i] != '1' {
+			break
+		}
+		b = append([]byte{0x00}, b...)
+	}
+
+	/* Verify checksum */
+	if len(b) < 5 {
+		return 0, nil, fmt.Errorf("Invalid base-58 check string: missing checksum.")
+	}
+
+	/* Create a new SHA256 context */
+	sha256_h := sha256.New()
+
+	/* SHA256 Hash #1 */
+	sha256_h.Reset()
+	sha256_h.Write(b[:len(b)-4])
+	hash1 := sha256_h.Sum(nil)
+
+	/* SHA256 Hash #2 */
+	sha256_h.Reset()
+	sha256_h.Write(hash1)
+	hash2 := sha256_h.Sum(nil)
+
+	/* Compare checksum */
+	if bytes.Compare(hash2[0:4], b[len(b)-4:]) != 0 {
+		return 0, nil, fmt.Errorf("Invalid base-58 check string: invalid checksum.")
+	}
+
+	/* Strip checksum bytes */
+	b = b[:len(b)-4]
+
+	/* Extract and strip version */
+	ver = b[0]
+	b = b[1:]
+
+	return ver, b, nil
 }
 
 // ToWIF converts a Bitcoin private key to a Wallet Import Format string.
 func (priv *PrivateKey) ToWIF() (wif string) {
 	/* See https://en.bitcoin.it/wiki/Wallet_import_format */
 
-	/* Create a new SHA256 context */
-	sha256_h := sha256.New()
-
-	/* Convert the private key to a byte sequence */
+	/* Convert the private key to big endian bytes */
 	priv_bytes := priv.D.Bytes()
 
-	/* 1. Prepend 0x80 */
-	wif_bytes := append([]byte{0x80}, priv_bytes...)
-
-	/* 2. SHA256 Hash */
-	sha256_h.Reset()
-	sha256_h.Write(wif_bytes)
-	priv_hash_1 := sha256_h.Sum(nil)
-
-	/* 3. SHA256 Hash */
-	sha256_h.Reset()
-	sha256_h.Write(priv_hash_1)
-	priv_hash_2 := sha256_h.Sum(nil)
-
-	/* 4. Checksum is first 4 bytes of second hash */
-	checksum := priv_hash_2[0:4]
-
-	/* 5. Append the checksum */
-	wif_bytes = append(wif_bytes, checksum...)
-
-	/* 6. Base58 the byte sequence */
-	wif = Base58(wif_bytes)
+	/* Convert bytes to base-58 check encoded string with version 0x80 */
+	wif = b58checkencode(0x80, priv_bytes)
 
 	return wif
 }
@@ -154,43 +235,23 @@ func (pub *PublicKey) ToAddress(version uint8) (address string) {
 	ripemd160_h := ripemd160.New()
 
 	/* Convert the public key to a byte sequence */
-	pub_bytes := pub.X.Bytes()
-	pub_bytes = append(pub_bytes, pub.Y.Bytes()...)
+	pub_bytes := append(pub.X.Bytes(), pub.Y.Bytes()...)
 
-	/* 1. Prepend 0x04 */
+	/* Prepend 0x04 */
 	pub_bytes = append([]byte{0x04}, pub_bytes...)
 
-	/* 2. SHA256 Hash */
+	/* SHA256 Hash */
 	sha256_h.Reset()
 	sha256_h.Write(pub_bytes)
 	pub_hash_1 := sha256_h.Sum(nil)
 
-	/* 3. RIPEMD-160 Hash */
+	/* RIPEMD-160 Hash */
 	ripemd160_h.Reset()
 	ripemd160_h.Write(pub_hash_1)
 	pub_hash_2 := ripemd160_h.Sum(nil)
 
-	/* Prepend version byte */
-	pub_hash_2 = append([]byte{version}, pub_hash_2...)
-
-	/* 4. SHA256 Hash */
-	sha256_h.Reset()
-	sha256_h.Write(pub_hash_2)
-	pub_hash_3 := sha256_h.Sum(nil)
-
-	/* 5. SHA256 Hash */
-	sha256_h.Reset()
-	sha256_h.Write(pub_hash_3)
-	pub_hash_4 := sha256_h.Sum(nil)
-
-	/* 6. Checksum is first 4 bytes of previous hash */
-	checksum := pub_hash_4[0:4]
-
-	/* 7. Add checksum to extended RIPEMD-160 hash */
-	address_bytes := append(pub_hash_2, checksum...)
-
-	/* 8. Base58 the byte sequence */
-	address = Base58(address_bytes)
+	/* Convert hash bytes to base58 check encoded sequence */
+	address = b58checkencode(version, pub_hash_2)
 
 	return address
 }
--- btckey/btckey_test.go
+++ btckey/btckey_test.go
@@ -7,11 +7,109 @@
 package btckey
 
 import (
+	"bytes"
 	"math/big"
 	"testing"
 )
 
-func TestConvert(t *testing.T) {
+type b58Test struct {
+	bytes   []byte
+	encoded string
+}
+
+var b58Vectors = []b58Test{
+	{[]byte{0x4e, 0x19}, "6wi"},
+	{[]byte{0x3a, 0xb7}, "5UA"},
+	{[]byte{0xae, 0x0d, 0xdc, 0x9b}, "5T3W5p"},
+	{[]byte{0x65, 0xe0, 0xb4, 0xc9}, "3c3E6L"},
+	{[]byte{0x25, 0x79, 0x36, 0x86, 0xe9, 0xf2, 0x5b, 0x6b}, "7GYJp3ZThFG"},
+	{[]byte{0x94, 0xb9, 0xac, 0x08, 0x4a, 0x0d, 0x65, 0xf5}, "RspedB5CMo2"},
+}
+
+func TestBase58(t *testing.T) {
+	/* Test base-58 encoding */
+	for i := 0; i < len(b58Vectors); i++ {
+		got := b58encode(b58Vectors[i].bytes)
+		if got != b58Vectors[i].encoded {
+			t.Fatalf("b58encode(%v): got %s, expected %s", b58Vectors[i].bytes, got, b58Vectors[i].encoded)
+		}
+	}
+	t.Log("success b58encode()")
+
+	/* Test base-58 decoding */
+	for i := 0; i < len(b58Vectors); i++ {
+		got, err := b58decode(b58Vectors[i].encoded)
+		if err != nil {
+			t.Fatalf("b58decode(%s): got error %v, expected %v", b58Vectors[i].encoded, err, b58Vectors[i].bytes)
+		}
+		if bytes.Compare(got, b58Vectors[i].bytes) != 0 {
+			t.Fatalf("b58decode(%s): got %v, expected %v", b58Vectors[i].encoded, got, b58Vectors[i].bytes)
+		}
+	}
+	t.Log("success b58decode()")
+
+	/* Test base-58 decoding of invalid strings */
+	b58InvalidVectors := []string{"5T3IW5p", "6Owi"}
+	for i := 0; i < len(b58InvalidVectors); i++ {
+		got, err := b58decode(b58InvalidVectors[i])
+		if err == nil {
+			t.Fatalf("b58decode(%s): got %v, expected error", b58InvalidVectors[i], got)
+		}
+		t.Logf("b58decode(%s): got expected err %v", b58InvalidVectors[i], err)
+	}
+	t.Log("success b58decode() handling of invalid strings")
+}
+
+type b58CheckTest struct {
+	ver     uint8
+	bytes   []byte
+	encoded string
+}
+
+var b58CheckVectors = []b58CheckTest{
+	{0x00, []byte{0x01, 0x09, 0x66, 0x77, 0x60, 0x06, 0x95, 0x3D, 0x55, 0x67, 0x43, 0x9E, 0x5E, 0x39, 0xF8, 0x6A, 0x0D, 0x27, 0x3B, 0xEE}, "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM"},
+	{0x80, []byte{0x0C, 0x28, 0xFC, 0xA3, 0x86, 0xC7, 0xA2, 0x27, 0x60, 0x0B, 0x2F, 0xE5, 0x0B, 0x7C, 0xAE, 0x11, 0xEC, 0x86, 0xD3, 0xBF, 0x1F, 0xBE, 0x47, 0x1B, 0xE8, 0x98, 0x27, 0xE1, 0x9D, 0x72, 0xAA, 0x1D}, "5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ"},
+}
+
+func TestBase58Check(t *testing.T) {
+	/* Test base-58 check encoding */
+	for i := 0; i < len(b58CheckVectors); i++ {
+		got := b58checkencode(b58CheckVectors[i].ver, b58CheckVectors[i].bytes)
+		if got != b58CheckVectors[i].encoded {
+			t.Fatalf("b58checkencode(0x%02x, %v): got %s, expected %s", b58CheckVectors[i].ver, b58CheckVectors[i].bytes, got, b58CheckVectors[i].encoded)
+		}
+	}
+	t.Log("success b58checkencode()")
+
+	/* Test base-58 check decoding */
+	for i := 0; i < len(b58CheckVectors); i++ {
+		ver, got, err := b58checkdecode(b58CheckVectors[i].encoded)
+		if err != nil {
+			t.Fatalf("b58checkdecode(%s): got error %v, expected ver %v, bytes %v", b58CheckVectors[i].encoded, err, b58CheckVectors[i].ver, b58CheckVectors[i].bytes)
+		}
+		if ver != b58CheckVectors[i].ver || bytes.Compare(got, b58CheckVectors[i].bytes) != 0 {
+			t.Fatalf("b58checkdecode(%s): got ver %v, bytes %v, expected ver %v, bytes %v", b58CheckVectors[i].encoded, ver, got, b58CheckVectors[i].ver, b58CheckVectors[i].bytes)
+		}
+	}
+	t.Log("success b58checkdecode()")
+
+	/* Test base-58 check decoding of invalid strings */
+	b58CheckInvalidVectors := []string{
+		"5T3IW5p", // Invalid base58
+		"6wi",     // Missing checksum
+		"6UwLL9Risc3QfPqBUvKofHmBQ7wMtjzm", // Invalid checksum
+	}
+	for i := 0; i < len(b58CheckInvalidVectors); i++ {
+		ver, got, err := b58checkdecode(b58CheckInvalidVectors[i])
+		if err == nil {
+			t.Fatalf("b58checkdecode(%s): got ver %v, bytes %v, expected error", b58CheckInvalidVectors[i], ver, got)
+		}
+		t.Logf("b58checkdecode(%s): got expected err %v", b58CheckInvalidVectors[i], err)
+	}
+	t.Log("success b58checkdecode() handling of invalid strings")
+}
+
+func TestExport(t *testing.T) {
 	/* Sample Private Key */
 	D := []byte{0x18, 0xE1, 0x4A, 0x7B, 0x6A, 0x30, 0x7F, 0x42, 0x6A, 0x94, 0xF8, 0x11, 0x47, 0x01, 0xE7, 0xC8, 0xE7, 0x74, 0xE7, 0xF9, 0xA4, 0x7E, 0x2C, 0x20, 0x35, 0xDB, 0x29, 0xA2, 0x06, 0x32, 0x17, 0x25}
 	/* Sample Corresponding Public Key */
