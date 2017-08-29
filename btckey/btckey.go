ec0e658486983068024d0eb0033aaf4415b4ac29
Andrei Terentiev <terentiev.andrei@gmail.com>
2017-08-29 20:53:59 +0900

    add-signing-capability

--- btckey/btckey.go
+++ btckey/btckey.go
@@ -8,6 +8,8 @@ package btckey
 import (
 	"bytes"
 	"crypto/sha256"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"fmt"
 	"io"
 	"math/big"
@@ -596,4 +598,34 @@ func (pub *PublicKey) ToAddressUncompressed() (address string) {
 	address = b58checkencode(0x00, pub_hash_2)
 
 	return address
+}
+
+func PrivateKeyFromInt(key int64) (*ecdsa.PrivateKey) {
+	prKey.D = big.NewInt(key)
+    prKey.PublicKey.Curve = elliptic.P256()
+    return prKey
+}
+
+// Sign signs arbitrary data using ECDSA.
+func Sign(data []byte, key int64) ([]byte, error) {
+	// hash message
+	privKey := PrivateKeyFromInt(key)
+	digest := sha256.Sum256(data)
+
+	// sign the hash
+	r, s, err := ecdsa.Sign(rand.Reader, privkey, digest[:])
+	if err != nil {
+		return nil, err
+	}
+
+	// encode the signature {R, S}
+	// big.Int.Bytes() will need padding in the case of leading zero bytes
+	params := privkey.Curve.Params()
+	curveOrderByteSize := params.P.BitLen() / 8
+	rBytes, sBytes := r.Bytes(), s.Bytes()
+	signature := make([]byte, curveOrderByteSize*2)
+	copy(signature[curveOrderByteSize-len(rBytes):], rBytes)
+	copy(signature[curveOrderByteSize*2-len(sBytes):], sBytes)
+
+	return signature, nil
 }
